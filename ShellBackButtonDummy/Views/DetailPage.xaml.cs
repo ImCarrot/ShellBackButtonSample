4548a1ff84bbc559bd82ac195e091eff82cfd749
Aditya Sharma <aditya.sharma23@outlook.com>
2017-08-17 23:35:54 +0530

    Add project files.

---
+++ ShellBackButtonDummy/Views/DetailPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ShellBackButtonDummy.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace ShellBackButtonDummy.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+
+        private void MyDummyControl_PrimaryButtonClicked()
+        {
+            if (ViewModel.NavigationService.CanGoBack)
+                ViewModel.NavigationService.GoBack();
+        }
+    }
+}
